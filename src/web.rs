6895951947d424720dea9c29c52f046bef122039
fireyy <fireyy@gmail.com>
2022-12-13 13:55:51 +0800

    fix(wasm): EventSource event not trigger

--- src/web.rs
+++ src/web.rs
@@ -1,21 +1,14 @@
 use crate::{EsEvent, EventHandler, Result};
-use gloo_events::EventListener;
 use wasm_bindgen_futures::spawn_local;
-use web_sys::Event;
 use web_sys::EventSource;
-use web_sys::EventSourceInit;
-use web_sys::MessageEvent;
+
+#[allow(clippy::needless_pass_by_value)]
+fn string_from_js_string(s: js_sys::JsString) -> String {
+    s.as_string().unwrap_or(format!("{:#?}", s))
+}
 
 fn new_event_source(url: &str) -> Result<EventSource> {
     EventSource::new(url).map_err(|_| "couldn't aquire event source".to_string())
-}
-
-fn new_event_source_with_credentials(url: &str, credentials: bool) -> Result<EventSource> {
-    EventSource::new_with_event_source_init_dict(
-        url,
-        EventSourceInit::new().with_credentials(credentials),
-    )
-    .map_err(|_| "couldn't aquire event source".to_string())
 }
 
 pub fn es_connect(url: String, on_event: EventHandler) -> Result<()> {
@@ -27,6 +20,7 @@ pub fn es_connect(url: String, on_event: EventHandler) -> Result<()> {
 }
 
 pub async fn es_connect_async(url: String, on_event: EventHandler) {
+    use wasm_bindgen::closure::Closure;
     use wasm_bindgen::JsCast as _;
 
     let es = new_event_source(&url).unwrap();
@@ -34,29 +28,40 @@ pub async fn es_connect_async(url: String, on_event: EventHandler) {
     let on_event: std::rc::Rc<dyn Send + Fn(EsEvent) -> std::ops::ControlFlow<()>> =
         on_event.into();
 
-    let on_event = on_event.clone();
-
     {
         let on_event = on_event.clone();
-        EventListener::new(&es, "error", move |event: &Event| {
-            let on_event = on_event.clone();
-            on_event(EsEvent::Error(format!("Error: {:?}", event)));
-        });
+        let onerror_callback = Closure::wrap(Box::new(move |error_event: web_sys::ErrorEvent| {
+            tracing::error!(
+                "error event: {}: {:?}",
+                error_event.message(),
+                error_event.error()
+            );
+            on_event(EsEvent::Error(error_event.message()));
+        }) as Box<dyn FnMut(web_sys::ErrorEvent)>);
+        es.set_onerror(Some(onerror_callback.as_ref().unchecked_ref()));
+        onerror_callback.forget();
     }
 
     {
         let on_event = on_event.clone();
-        EventListener::new(&es, "open", move |_event: &Event| {
+        let onopen_callback = Closure::wrap(Box::new(move |_| {
             on_event(EsEvent::Opened);
-        });
+        }) as Box<dyn FnMut(wasm_bindgen::JsValue)>);
+        es.set_onopen(Some(onopen_callback.as_ref().unchecked_ref()));
+        onopen_callback.forget();
     }
 
     {
-        let on_event = on_event.clone();
-        EventListener::new(&es, "message", move |event: &Event| {
-            let event = event.dyn_ref::<MessageEvent>().unwrap();
-            let text = event.data().as_string().expect("expect text data");
-            on_event(EsEvent::Message(text));
-        });
+        let onmessage_callback = Closure::wrap(Box::new(move |e: web_sys::MessageEvent| {
+            // Handle
+            if let Ok(txt) = e.data().dyn_into::<js_sys::JsString>() {
+                on_event(EsEvent::Message(string_from_js_string(txt)));
+            }
+        }) as Box<dyn FnMut(web_sys::MessageEvent)>);
+        // set message event handler on Eventsource
+        es.set_onmessage(Some(onmessage_callback.as_ref().unchecked_ref()));
+
+        // forget the callback to keep it alive
+        onmessage_callback.forget();
     }
 }
